15c9decb7fe4525ddf38088a8d4d57e9faa310ad
Shizhe Chen <shizhe95@gmail.com>
2015-03-19 16:20:42 -0400

    malloc

---
+++ list.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include "./list.h"
+
+/**
+ * pushFront
+ * Takes in ptr to a list and data to add
+ * Should add new node containing the data to the head of the list, and increment size
+ */
+void pushFront(LIST *list, int data)
+{
+	// your code here
+}
+
+/**
+ * pushFront
+ * Takes in ptr to a list and data to add
+ * Should add new node containing the data to the tail of the list, and increment size
+ */
+void pushBack(LIST *list, int data)
+{
+	// your code here
+}
+
+/**
+ * popFront
+ * Takes in ptr to a list
+ * Remove and free node at the front of the list, and decrement size
+ * Return the value of that node
+ * Return 0 if the size of the list is 0
+ */
+int popFront(LIST *list)
+{
+	// your code here
+	return 0;
+}
+
+/**
+ * popBack
+ * Takes in ptr to a list
+ * Remove and free node at the back of the list, and decrement size
+ * Return the value of that node
+ * Return 0 if the size of the list is 0
+ */
+int popBack(LIST *list)
+{
+	// your code here
+	return 0;
+}
+
+
